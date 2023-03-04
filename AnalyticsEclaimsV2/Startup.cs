5a5f5b62bacc18028b67a1e7c62c3bb213bae1d2
Kliad <86932147+Kliad@users.noreply.github.com>
2023-03-05 05:27:26 +0800

    SHELL

---
+++ AnalyticsEclaimsV2/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Xml.Linq;
+
+[assembly: FunctionsStartup(typeof(AnalyticsEclaimsV2.Startup))]
+
+namespace AnalyticsEclaimsV2
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddSingleton<IMyService>((s) => {
+                return new MyService();
+            });
+
+            builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        }
+    }
+}
